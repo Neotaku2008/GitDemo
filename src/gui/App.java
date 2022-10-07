debf40c52e75959e6b8df384e7592462a579557c
Neotaku2008 <hugo.emata@gmail.com>
2022-10-06 20:35:07 -0600

    American guy commit

--- src/gui/App.java
+++ src/gui/App.java
@@ -10,7 +10,10 @@ public class App {
 				new MainFrame();
 				System.out.println("Line 1");
 				System.out.println("Line 2");
-				System.out.println("Line 3");
+				System.out.println("Line 3");				
+				System.out.println("Line 4");
+				System.out.println("Line 5");
+				System.out.println("Line 6");								
 			}
 		});
 				

26f2d7b8bebf9d4845a10cda8eb3ffef9de7aa2c
Neotaku2008 <hugo.emata@gmail.com>
2022-10-06 22:48:47 -0600

    Architect Asian development

--- src/gui/App.java
+++ src/gui/App.java
@@ -13,7 +13,10 @@ public class App {
 				System.out.println("Line 3");				
 				System.out.println("Line 4");
 				System.out.println("Line 5");
-				System.out.println("Line 6");								
+				System.out.println("Line 6");
+				System.out.println("Line 7");
+				System.out.println("Line 8");
+				System.out.println("Line 9");					
 			}
 		});
 				
